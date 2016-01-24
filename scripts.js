7e062b48e3fdb62ef033e7b3e573bb1e1e166b23
Jeremy Miragliotta <jdmiragliotta@gmail.com>
2016-01-23 20:58:28 -0500

    Practice with dot and bracket notation

--- scripts.js
+++ scripts.js
@@ -1,3 +1,14 @@
 //Use this file to implement Part One of your project
+var animal = {};
 
+animal.username = "Bob";
+//console.log(animal.username);
 
+animal["tagline"] = "Hi I am Bob";
+//console.log(animal["tagline"])
+
+var noises = [];
+
+animal["noises"] = noises;
+
+console.log(animal);
