61182c60c3a123385c0a3d4daa019ad16f8598ec
Jeremy Miragliotta <jdmiragliotta@gmail.com>
2016-01-23 21:15:06 -0500

    Practice with for in loop

--- scripts.js
+++ scripts.js
@@ -3,12 +3,18 @@ var animal = {};
 
 animal.username = "Bob";
 //console.log(animal.username);
-
 animal["tagline"] = "Hi I am Bob";
 //console.log(animal["tagline"])
-
 var noises = [];
+animal.noises = noises;
+//console.log(animal);
 
-animal["noises"] = noises;
-
-console.log(animal);
+var count = 0;
+for(var key in animal){
+  count++;
+  if (key === "username"){
+    console.log("Hi my name is "+ animal[key]);
+  }else if (key === "tagline"){
+    console.log("I like to say "+ animal[key]);
+  }
+}
